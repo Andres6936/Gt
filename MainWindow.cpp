7bdd7c22665fb76a029bbe33b0e6970703420eb2
Joan Andrés <andres6936@live.com>
2020-10-20 10:22:09 -0500

    Change the standard used, from C++14 to C++17

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,11 +2,29 @@
 
 #include "MainWindow.hpp"
 
+#include <filesystem>
+
 using namespace Gt;
+
 
 MainWindow::MainWindow()
 	: QMainWindow(), fileTableWidget(new FileTableWidget())
 {
 	setCentralWidget(fileTableWidget);
 	setWindowTitle(tr("Git Explorer File"));
+
+	startUp();
+}
+
+void MainWindow::startUp() const noexcept
+{
+	namespace fs = std::filesystem;
+
+	for (const fs::directory_entry& directory: fs::directory_iterator("/"))
+	{
+		const fs::path& path = directory.path();
+
+		fileTableWidget->addEntry(QString::fromStdString(path.filename().string()),
+				QString::fromStdString(path.relative_path()));
+	}
 }
