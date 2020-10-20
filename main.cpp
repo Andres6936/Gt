2282a6a30fbd65afd69b51bc83df3b9b443adc75
Joan Andrés <andres6936@live.com>
2020-10-20 00:32:55 -0500

    Change the ugly style of theme of Qt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 
 #include <QApplication>
+#include <QStyleFactory>
+
 #include "MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	a.setStyle(QStyleFactory::create("Fusion"));
 	Gt::MainWindow window {};
 	window.show();
 	return a.exec();
