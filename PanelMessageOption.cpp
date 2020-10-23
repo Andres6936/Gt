00f09a0fe9b8ec2fddcc14354c6cee90dfcb711c
Joan Andrés <andres6936@live.com>
2020-10-23 18:32:06 -0500

    Added commit button

--- PanelMessageOption.cpp
+++ PanelMessageOption.cpp
@@ -3,12 +3,16 @@
 #include "PanelMessageOption.hpp"
 
 #include <QHBoxLayout>
+#include <QPushButton>
 
 using namespace Gt;
 
 PanelMessageOption::PanelMessageOption(QWidget* parent) noexcept : QWidget(parent)
 {
 	auto mainLayout = new QHBoxLayout(this);
+	auto commitButton = new QPushButton("Commit", this);
+
+	mainLayout->addWidget(commitButton);
 
 	this->setLayout(mainLayout);
 }
