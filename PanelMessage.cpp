c2733f36cf4a1d07382f21a89fe8883ec4d5a1c8
Joan Andrés <andres6936@live.com>
2020-10-23 18:35:19 -0500

    Added panel options to panel message

--- PanelMessage.cpp
+++ PanelMessage.cpp
@@ -1,6 +1,7 @@
 // Joan Andrés (@Andres6936) Github.
 
 #include "PanelMessage.hpp"
+#include "PanelMessageOption.hpp"
 
 #include <QTextEdit>
 #include <QVBoxLayout>
@@ -9,11 +10,13 @@ using namespace Gt;
 
 PanelMessage::PanelMessage(QWidget* parent) noexcept : QWidget(parent)
 {
+	auto panelOptions = new PanelMessageOption(this);
 	auto mainLayout = new QVBoxLayout(this);
 	auto message = new QTextEdit(this);
 	message->setPlaceholderText("Commit Message");
 
 	mainLayout->addWidget(message);
+	mainLayout->addWidget(panelOptions);
 
 	this->setLayout(mainLayout);
 }
