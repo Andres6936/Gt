736fc90c2494b8b55c5fa430d348cb8ffa2a3089
Joan Andrés <andres6936@live.com>
2020-10-23 12:14:52 -0500

    Support to Lib Git API

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <QApplication>
 #include <QStyleFactory>
 
+#include <git2.h>
+
 #include "MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
+	git_libgit2_init();
+
 	QApplication a(argc, argv);
 	a.setStyle(QStyleFactory::create("Fusion"));
 	Gt::MainWindow window {};
 	window.show();
 	return a.exec();
+
+	git_libgit2_shutdown();
 }
