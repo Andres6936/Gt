c32c3ffc1d8ad9fafa3f00ec04626d481f75289f
Joan Andrés <andres6936@live.com>
2020-10-23 19:33:22 -0500

    Added CheckBox and ToolButton to layout

--- PanelMessageOption.cpp
+++ PanelMessageOption.cpp
@@ -3,21 +3,32 @@
 #include "PanelMessageOption.hpp"
 
 #include <QMenu>
+#include <QCheckBox>
 #include <QHBoxLayout>
 #include <QPushButton>
+#include <QToolButton>
 
 using namespace Gt;
 
 PanelMessageOption::PanelMessageOption(QWidget* parent) noexcept : QWidget(parent)
 {
 	auto mainLayout = new QHBoxLayout(this);
-	auto commitButton = new QPushButton("Commit", this);
+
 	auto commitAndPush = new QMenu( this);
 	commitAndPush->addAction("Commit and Push");
 
+	auto commitButton = new QPushButton("Commit", this);
 	commitButton->setMenu(commitAndPush);
 
+	auto amendCheckBox = new QCheckBox("Amend", this);
+
+	auto commitMessageHistory = new QToolButton(this);
+	auto showCommitOptions = new QToolButton(this);
+
 	mainLayout->addWidget(commitButton);
+	mainLayout->addWidget(amendCheckBox);
+	mainLayout->addWidget(commitMessageHistory);
+	mainLayout->addWidget(showCommitOptions);
 
 	this->setLayout(mainLayout);
 }
